87cbe3290abcf1b8ccd441ee183c788fde325bff
pjungjs <pjungjs@gmail.com>
2023-02-28 20:44:54 -0500

    passed all the tests. used routes, navlink, navigate.

--- src/components/pets/PetsList.js
+++ src/components/pets/PetsList.js
@@ -2,7 +2,8 @@ import PetsListNav from "./PetsListNav";
 import Pet from "./Pet";
 import "./PetsList.css";
 
-export const PetsList = ({ pets }) => {
+export const PetsList = ({ pets, kind }) => {
+  const animal = pets.filter(pet => pet.kind === kind);
   const [cats, dogs] = pets.reduce(
     (acc, pet) => {
       const position = pet.kind === "Cat" ? 0 : 1;
@@ -16,15 +17,28 @@ export const PetsList = ({ pets }) => {
     <section className="pets-wrapper">
       <PetsListNav cats={cats} dogs={dogs} />
       <section className="pets-list">
+        {
+          kind === "cat"
+          ? cats.map((cat) => <Pet key={cat.id} kind="cat" pet={cat} />)
+          : kind === "dog"
+            ? dogs.map((dog) => <Pet key={dog.id} kind="dog" pet={dog} />)
+            : null
+        }
+
+        {/* All pets */}
+        {/* {animal.map((pet) => {
+          <Pet key={pet.id} kind={pet.kind} pet={pet} />
+        })} */}
+
         {/* All cats section */}
-        {cats.map((cat) => (
+        {/* {cats.map((cat) => (
           <Pet key={cat.id} kind="cat" pet={cat} />
-        ))}
+        ))} */}
 
         {/* All dogs section */}
-        {dogs.map((dog) => (
+        {/* {dogs.map((dog) => (
           <Pet key={dog.id} kind="dog" pet={dog} />
-        ))}
+        ))} */}
       </section>
     </section>
   );
