e413084030e2d87b336928749d6d21fdaa4193f7
SamuelBAlba <brayanbalba24@gmail.com>
2023-02-28 21:51:13 -0500

    done

--- src/components/common/Nav.js
+++ src/components/common/Nav.js
@@ -1,22 +1,37 @@
 import "./Nav.css";
+// import link
+import { Link } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
+
+
 
 function Nav() {
   return (
     <header>
       <article>
         <h1>
-          <a href="/">
-            Northwest <span>Animal Hospital</span>
-          </a>
+          <Link to="/">
+          Northwest <span>Animal Hospital</span>
+          </Link>
+          {/* change a tags to use Link */}
+          {/* <a href="/"> */}
+            {/* Northwest <span>Animal Hospital</span> */}
+          {/* </a> */}
         </h1>
       </article>
       <aside>
         <ul>
           <li>
-            <a href="/staff">All Staff</a>
+            <Link  to="/staff">
+            All Staff
+            </Link>
+            {/* <a href="/staff">All Staff</a> */}
           </li>
           <li>
-            <a href="/pets">All Pets</a>
+            {/* <a href="/pets">All Pets</a> */}
+            <Link to="/pets">
+            All Pets
+            </Link>
           </li>
         </ul>
       </aside>
