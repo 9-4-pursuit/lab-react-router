8fd1a641b73ac85c868a58c7feb006fef63104a6
Amina-Moufakkir <aminamouf22@gmail.com>
2023-02-28 20:19:41 -0500

    react-router-lab comlete

--- src/components/pets/PetsList.js
+++ src/components/pets/PetsList.js
@@ -1,8 +1,12 @@
 import PetsListNav from "./PetsListNav";
+import { useParams } from "react-router-dom";
+
 import Pet from "./Pet";
 import "./PetsList.css";
 
 export const PetsList = ({ pets }) => {
+  const { kind } = useParams();
+
   const [cats, dogs] = pets.reduce(
     (acc, pet) => {
       const position = pet.kind === "Cat" ? 0 : 1;
@@ -17,14 +21,10 @@ export const PetsList = ({ pets }) => {
       <PetsListNav cats={cats} dogs={dogs} />
       <section className="pets-list">
         {/* All cats section */}
-        {cats.map((cat) => (
-          <Pet key={cat.id} kind="cat" pet={cat} />
-        ))}
 
-        {/* All dogs section */}
-        {dogs.map((dog) => (
-          <Pet key={dog.id} kind="dog" pet={dog} />
-        ))}
+        {kind === "cats"
+          ? cats.map((cat) => <Pet key={cat.id} kind="cat" pet={cat} />)
+          : dogs.map((dog) => <Pet key={dog.id} kind="dog" pet={dog} />)}
       </section>
     </section>
   );
