d3d3f0bbc10886f4dc9ff488dba0e556aded2c33
Adrian Burke <114108495+AdrianBurke1@users.noreply.github.com>
2023-03-06 21:21:48 -0500

    lab-react-router

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
 import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
-
-// Comp section
-import Home from "./components/home/Home";
 import Nav from "./components/common/Nav";
 import Footer from "./components/common/Footer";
-import PetsList from "./components/pets/PetsList";
+import Home from "./components/home/Home";
 import StaffList from "./components/staff/StaffList";
-
-// Data section
-// Note: this data is pulled from an API in most cases
-import { petData } from "./data/pets";
-import { ownerData } from "./data/owners";
+import PetsList from "./components/pets/PetsList";
 import { employeeData } from "./data/employees.js";
-
+import { ownerData } from "./data/owners";
+import { petData } from "./data/pets";
 function App() {
   const [employees] = useState(employeeData);
   const [owners] = useState(ownerData);
   const [pets] = useState(petData);
-
   return (
     <div className="wrapper">
       <Nav />
+
       <Routes>
         <Route
           path="/"
           element={<Home employees={employees} owners={owners} pets={pets} />}
         />
+
         <Route path="/staff" element={<StaffList employees={employees} />} />
+
         <Route path="/pets">
           <Route index element={<PetsList pets={pets} />} />
           <Route
@@ -38,8 +34,10 @@ function App() {
             path="dogs"
             element={<PetsList pets={pets} petType={"dog"} />}
           />
+          
         </Route>
       </Routes>
+
       <Footer />
     </div>
   );
