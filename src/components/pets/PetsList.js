3d0945a7b366dd8c536557507f100ed456aa3aa0
Adrian Burke <114108495+AdrianBurke1@users.noreply.github.com>
2023-03-06 21:33:08 -0500

    lab-react-router

--- src/components/pets/PetsList.js
+++ src/components/pets/PetsList.js
@@ -1,41 +1,53 @@
 import PetsListNav from "./PetsListNav";
-import Pet from "./Pet";
-import "./PetsList.css";
-import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+ import Pet from "./Pet";
+ import "./PetsList.css";
+ import { useNavigate } from "react-router-dom";
+ import { useEffect } from "react";
 
-export const PetsList = ({ pets }) => {
-  let navigate = useNavigate();
 
-  const [cats, dogs] = pets.reduce(
-    (acc, pet) => {
-      const position = pet.kind === "Cat" ? 0 : 1;
+ export const PetsList = ({ pets, petType }) => {
+
+   let navigate = useNavigate();
+
+   const [cats, dogs] = pets.reduce(
+     (acc, pet) => {
+       const position = pet.kind === "Cat" ? 0 : 1;
       acc[position].push(pet);
       return acc;
     },
-    [[], []]
-  );
+     [[], []]
+   );
 
-  useEffect(() => {
-    if (!petType) {
-      navigate("/pets/cats");
-    }
-  });
+   useEffect(() => {
+     if (!petType) {
+       navigate("/pets/cats")
+     }
+   })
 
-  function catDog() {
-    return petType === "cat" ? cats : dogs;
-  }
+   function catOrDog() {
+     return (petType === "cat") ? cats : dogs;
+   }
 
-  return (
-    <section className="pets-wrapper">
-      <PetsListNav cats={cats} dogs={dogs} />
-      <section className="pets-list">
-        {catOrDog().map((pet) => (
-          <Pet key={pet.id} kind={pet.kind.toLowerCase()} pet={pet} />
-        ))}
-      </section>
-    </section>
-  );
+   return (
+     <section className="pets-wrapper">
+       <PetsListNav cats={cats} dogs={dogs} />
+       <section className="pets-list">
+         {/* Specific pet section */}
+         {(catOrDog().map((pet) => (
+           <Pet key={pet.id} kind={pet.kind.toLowerCase()} pet={pet} />
+         )))}
+       </section>
+     </section>
+   );
+
+    
+          
+            
+    
+
+          
+    
+    
+  
 };
-
 export default PetsList;
