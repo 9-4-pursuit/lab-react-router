00ebd8ada77aee2db54d1ac6341445b149674767
Wilsantos1975 <Faustosantos@pursuit.org>
2023-03-10 01:10:41 -0500

    lab completed long time ago

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Footer from "./components/common/Footer";
 import Home from "./components/home/Home";
 import StaffList from "./components/staff/StaffList";
 import PetsList from "./components/pets/PetsList";
+import { Routes, Route } from "react-router-dom";
 
 /*
   Data
@@ -26,10 +27,16 @@ function App() {
   return (
     <div className="wrapper">
       <Nav />
-      <Home employees={employees} owners={owners} pets={pets} />
-      <StaffList employees={employees} />
-      <PetsList pets={pets} />
-      <Footer />
+
+      <Routes>
+        <Route
+          path="/"
+          element={<Home employees={employees} owners={owners} pets={pets} />}
+        />
+        <Route path="/staff" element={<StaffList employees={employees} />} />
+        <Route path="/pets" element={<PetsList pets={pets} />} />
+        <Route path="/footer" element={<Footer />} />
+      </Routes>
     </div>
   );
 }
